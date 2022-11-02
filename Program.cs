d24526de82174671704ede56c41f5ea259dd82b0
weslley.souza@soupromove.com.br <weslley.souza@soupromove.com.br>
2022-11-02 17:31:18 -0300

    Finalizado desafio da DIO Reserva de Hotel

--- Program.cs
+++ Program.cs
@@ -2,24 +2,28 @@
 using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
-
+Console.WriteLine("Bem vindo ao hotel: Águas Formosas.");
+Console.WriteLine("-----------------------------------------------------------------");
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p1 = new Pessoa(nome: "Weslley", "Souza");
+Pessoa p2 = new Pessoa(nome: "Viviane", "Santos");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 3, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Nome dos Hóspedes: {p1.NomeCompleto}");
+Console.WriteLine($"Nome dos Hóspedes: {p2.NomeCompleto}");
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+Console.WriteLine($"Tipo da Suíte: {suite.TipoSuite}");
